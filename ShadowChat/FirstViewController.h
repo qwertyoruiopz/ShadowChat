c65ecbddfbac3f8b34553a09cdfbc0c5a196fe19
StarProject <info@qwertyoruiop.com>
2011-09-05 01:23:23 +0200

    Initial Commit

---
+++ ShadowChat/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  ShadowChat
+//
+//  Created by qwerty or on 05/09/11.
+//  Copyright 2011 uiop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ ShadowChat/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ShadowChat
+//
+//  Created by qwerty or on 05/09/11.
+//  Copyright 2011 uiop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
