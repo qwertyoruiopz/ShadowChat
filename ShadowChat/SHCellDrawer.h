7195ff1e56b1e148b9fc97216298dfbd3b295282
Maximus <imaximusx@gmail.com>
2011-11-16 19:26:34 -0500

    ---- didn't finish what i was implementing. Sorry need to work onhmwk.

---
+++ ShadowChat/SHCellDrawer.h
@@ -0,0 +1,13 @@
+//
+//  SHCellDrawer.h
+//  ShadowChat
+//
+//  Created by Max Shavrick on 11/16/11.
+//  Copyright (c) 2011 uiop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHCellDrawer : UIView
+
+@end
---
+++ ShadowChat/SHCellDrawer.m
@@ -0,0 +1,31 @@
+//
+//  SHCellDrawer.m
+//  ShadowChat
+//
+//  Created by Max Shavrick on 11/16/11.
+//  Copyright (c) 2011 uiop. All rights reserved.
+//
+
+#import "SHCellDrawer.h"
+
+@implementation SHCellDrawer
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
