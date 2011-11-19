8385acd40ba2907c8a4da5f6e17f744567c45db0
Maximus <imaximusx@gmail.com>
2011-11-18 19:40:25 -0500

    nothing SIRIous! ah i'm so funny... >_>

---
+++ ShadowChat/SHDrawerButton.m
@@ -0,0 +1,13 @@
+//
+//  SHDrawerButton.m
+//  ShadowChat
+//
+//  Created by Max Shavrick on 11/16/11.
+//  Copyright (c) 2011 uiop. All rights reserved.
+//
+
+#import "SHDrawerButton.h"
+
+@implementation SHDrawerButton
+
+@end
