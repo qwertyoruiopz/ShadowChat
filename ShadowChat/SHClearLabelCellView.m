7349975cc8be172d1caac377eea2b77b48cc8afb
Maximus <imaximusx@gmail.com>
2011-11-28 21:24:54 -0500

    Chat view seems to work on iPad and iPhone. The input bar positions properly. Forcing rotation by using other views seems to be where the issues are, the text view surpasses the view by like idk .. 90-ish pixels. Some if statements may fix this, or a new concept using some more simple maths. Maybe take the size of the screen, subtract the keyboard, and then the size of the bar? idk. The keyboard size changes from ipad-iphone also, so can not forget this. thanks :P <3

--- ShadowChat/SHClearLabelCellView.m
+++ ShadowChat/SHClearLabelCellView.m
@@ -2,8 +2,7 @@
 //  ClearLabelsCellView.m
 //  ShadowedTableView
 //
-//  Created by Matt Gallagher on 2009/08/21.
-//  Copyright 2009 Matt Gallagher. All rights reserved.
+// <3
 //
 
 #import "SHClearLabelCellView.h"
